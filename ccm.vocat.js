e21cf8a3bce80f78b5d4a858e816d227572d4ff7
derdanielb <d.brilz@gmx.net>
2016-06-19 12:27:42 +0200

    loaded tree structure in main component, restructured images in tree component, rework paths in tree component

--- ccm.vocat.js
+++ ccm.vocat.js
@@ -3,7 +3,7 @@ ccm.component({
     config: {
         html: [ccm.store, {local: 'template.json'}],
         jquery_ui: [ccm.load, 'https://code.jquery.com/ui/1.12.0-rc.2/jquery-ui.min.js'],
-        tree:[ccm.load, 'ccm_tree/ccm.brlz_tree.js']
+        tree:[ccm.instance, 'ccm_tree/ccm.brlz_tree.js', {element: jQuery('#tree_area_3'), dir: 'ccm_tree/demo.json'}]
     },
     Instance: function () {
 
@@ -24,8 +24,18 @@ ccm.component({
             var element = ccm.helper.element(this);
             element.html(ccm.helper.html(that.html.get('main')));
             var newtest = JSON.parse(JSON.stringify(folder));
+            this.tree.render();
+            //next two lines also a working alternative
+            //this.atree = ccm.instance('ccm_tree/ccm.brlz_tree.js', {element: jQuery('#tree_area_3'), dir: 'ccm_tree/demo.json'});
+            //this.atree.render();
 
-            ccm.helper.find(that, '#' + parent).append(ccm.helper.html(newtest));
+
+            //ccm.helper.find(that, '#' + parent).append(ccm.helper.html(newtest));
+            //element.tree(ccm.render({element: jQuery('#tree_area_3'), dir: 'demo.json'}));
+            //ccm.render( 'ccm_tree',{element: jQuery('#tree_area_3'), dir: 'ccm_tree/demo.json'});
+            //ccm.render(('ccm_tree/ccm.brlz_tree.js', {element: jQuery('#tree_area_3'), dir: 'demo.json'}));
+            //var treec = ccm.helper.element(tree);
+            //treec.render({element: jQuery('#tree_area_3'), dir: 'demo.json'});
 
 
 
