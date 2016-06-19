39c29a34f48cd9b03e15cbe752c19d8ea91a4c7d
derdanielb <d.brilz@gmx.net>
2016-06-19 14:24:03 +0200

    parent component clean up, adjusted some paths for vocab

--- ccm.vocat.js
+++ ccm.vocat.js
@@ -1,57 +1,48 @@
 ccm.component(
     {
-    name: 'vocat',
-    config: {
-        html: [ccm.store, {local: 'template.json'}],
-        jquery_ui: [ccm.load, 'https://code.jquery.com/ui/1.12.0-rc.2/jquery-ui.min.js'],
-        tree:[ccm.instance, 'ccm_tree/ccm.brlz_tree.js']
-    },
-    Instance: function () {
-        var currentTreeDir = "demo.json";
-        function refreshTitle(title){
-            document.title = "VOCAT: "+title;
+        name: 'vocat',
+        config: {
+            html: [ccm.store, {local: 'template.json'}],
+            jquery_ui: [ccm.load, 'https://code.jquery.com/ui/1.12.0-rc.2/jquery-ui.min.js'],
+            tree: [ccm.instance, 'ccm_tree/ccm.brlz_tree.js']
+        },
+        Instance: function () {
+            var currentTreeDir = "demo.json";
+
+            function refreshTitle(title) {
+                document.title = "VOCAT: " + title;
+            }
+
+            //tree:[ccm.instance, 'ccm_tree/ccm.brlz_tree.js', {element: jQuery('#tree_area_3'), dir: 'demo.json', whatacall: '{ alert("hi"); tree.render()}'}]
+
+            //https://code.jquery.com/ui/1.11.4/jquery-ui.min.js
+            this.render = function () {
+                var that = this;
+                var reloadvocab = function (voc) {
+
+                    currentTreeDir = voc;
+                    console.log(voc);
+                    that.atree = ccm.instance('ccm_tree/ccm.brlz_tree.js', {
+                        element: jQuery('#tree_area_2'),
+                        dir: currentTreeDir,
+                        whatacall: reloadvocab
+                    });
+                    that.atree.render();
+                    refreshTitle(voc);
+                };
+                var element = ccm.helper.element(this);
+                element.html(ccm.helper.html(that.html.get('main')));
+                //this.tree.render();
+                //next two lines also a working alternative
+                this.atree = ccm.instance('ccm_tree/ccm.brlz_tree.js', {
+                    element: jQuery('#tree_area_2'),
+                    dir: currentTreeDir,
+                    whatacall: reloadvocab
+                });
+                this.atree.render();
+            }
         }
-
-        //tree:[ccm.instance, 'ccm_tree/ccm.brlz_tree.js', {element: jQuery('#tree_area_3'), dir: 'demo.json', whatacall: '{ alert("hi"); tree.render()}'}]
-
-        //https://code.jquery.com/ui/1.11.4/jquery-ui.min.js
-        this.render = function () {
-            var that = this;
-            //var folder = {
-            //    "tag": "div",
-            //    "class": "folder",
-            //    "id": "%placeholder%",
-            //    "inner": ""
-            //};
-            var reloadvocab = function(voc){
-
-                currentTreeDir = voc;
-                console.log(voc);
-                that.atree = ccm.instance('ccm_tree/ccm.brlz_tree.js', {element: jQuery('#tree_area_2'), dir: currentTreeDir, whatacall:reloadvocab});
-                that.atree.render();
-                refreshTitle(voc);
-            };
-            var element = ccm.helper.element(this);
-            element.html(ccm.helper.html(that.html.get('main')));
-            //var newtest = JSON.parse(JSON.stringify(folder));
-            //this.tree.render();
-            //next two lines also a working alternative
-            this.atree = ccm.instance('ccm_tree/ccm.brlz_tree.js', {element: jQuery('#tree_area_2'), dir: currentTreeDir, whatacall:reloadvocab});
-            this.atree.render();
-            //, whatacall:reloadvocab.bind(null)
-
-            //ccm.helper.find(that, '#' + parent).append(ccm.helper.html(newtest));
-            //element.tree(ccm.render({element: jQuery('#tree_area_3'), dir: 'demo.json'}));
-            //ccm.render( 'ccm_tree',{element: jQuery('#tree_area_3'), dir: 'ccm_tree/demo.json'});
-            //ccm.render(('ccm_tree/ccm.brlz_tree.js', {element: jQuery('#tree_area_3'), dir: 'demo.json'}));
-            //var treec = ccm.helper.element(tree);
-            //treec.render({element: jQuery('#tree_area_3'), dir: 'demo.json'});
-
-
-
-        }
-    }
-});
+    });
 
 
 //Data structure
